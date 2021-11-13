d9585e7d6dce009b1472ae502b393688d1516c36
Alexander <aleksandr__sokol@mail.ru>
2021-11-13 17:43:58 +0500

    feat: Add select for spouse

--- family_tree/serializer.py
+++ family_tree/serializer.py
@@ -1,5 +1,11 @@
+from django.db.models import CharField
 from rest_framework import serializers
+from rest_framework.fields import Field
+
 from .models import Person, Position
+
+from django.core.exceptions import ObjectDoesNotExist
+
 
 
 class PositionSerializer(serializers.ModelSerializer):
@@ -10,18 +16,39 @@ class PositionSerializer(serializers.ModelSerializer):
 
 class PersonSerializer(serializers.ModelSerializer):
     position = PositionSerializer(many=False, read_only=False)
+    mother_num = serializers.CharField(write_only=True)
+    father_num = serializers.CharField(write_only=True)
+    spouse_num = serializers.CharField(write_only=True)
 
     class Meta:
         model = Person
-        fields = ('id', 'name', 'family', 'position', 'mother', 'father', 'gender')
+        fields = ('id', 'name', 'family', 'position', 'mother', 'father', 'gender', 'mother_num', 'father_num', 'spouse_num')
         depth = 2
 
     def create(self, validated_data):
         validated_data_copy = validated_data.copy()
         position = validated_data_copy.pop('position')
+        mother_id = validated_data_copy.pop('mother_num')
+        father_id = validated_data_copy.pop('father_num')
+        spouse_id = validated_data_copy.pop('spouse_num')
+        try:
+            mother = Person.objects.get(pk=mother_id)
+        except ObjectDoesNotExist:
+            mother = None
+        try:
+            father = Person.objects.get(pk=father_id)
+        except ObjectDoesNotExist:
+            father = None
+        try:
+            spouse = Person.objects.get(pk=spouse_id)
+        except ObjectDoesNotExist:
+            spouse = None
         position_filter = Position.objects.get_or_create(country=position['country'],
                                                          sity=position['sity'],
                                                          )
+        validated_data_copy['mother'] = mother
+        validated_data_copy['father'] = father
+        validated_data_copy['current_spouse'] = spouse
         validated_data_copy['position'] = position_filter[0]
         person = Person.objects.create(**validated_data_copy)
         person.save()
--- templates/static/js/application.js
+++ templates/static/js/application.js
@@ -1,24 +1,8 @@
 $(document).ready(function() {
 
-    console.log('start Application')
-
-    // закрытии модального окна
-    $(document).on('click', '.modal_close_window', function (e) {
-        $(this).parent().parent().parent().parent().hide()
-    })
-
-    //    Открытие окна добавление человека
-    $(document).on('click', '.person', function () {
-        // Список доступных стран городов
-        console.log('start click person')
+    function get_countries(){
         let url = 'https://raw.githubusercontent.com/David-Haim/CountriesToCitiesJSON/master/countriesToCities.json';
         let country_dict;
-        let sity_dict;
-        let birthplace_country_select = document.getElementById('birthplace_country_select');
-        let birthplace_sity_select = document.getElementById('birthplace_sity_select');
-        $("#birthplace_country_select").empty();  // Очистка при повторном открытии окна
-        $("#birthplace_sity_select").empty();
-
         $.ajax({
 	       type: 'GET',
 	       url: url,
@@ -29,6 +13,63 @@ $(document).ready(function() {
                  country_dict =  JSON.parse(data)
 	       }
 	    });
+	    return country_dict;
+    }
+
+    function get_person_by_gender(G){
+        data = {}
+        if (G != 'None')
+            data = { 'gender': G}
+        let url = '/person';
+        let person_dict;
+        $.ajax({
+	       type: 'GET',
+	       url: url,
+	       data: jQuery.param(data) ,
+           async: false,
+	       processData: false,  // tell jQuery not to process the data
+           contentType: false,  // tell jQuery not to set contentType
+	       success: function(data, status){
+                 person_dict =  data
+	       }
+	    });
+	    return person_dict;
+    }
+
+    console.log('start Application')
+
+    // закрытии модального окна
+    $(document).on('click', '.modal_close_window', function (e) {
+        $(this).parent().parent().parent().parent().hide()
+    })
+
+    //    Открытие окна добавление человека
+    $(document).on('click', '.person', function () {
+        // Список доступных стран городов
+        let country_dict = get_countries();
+        let sity_dict;
+        let birthplace_country_select = document.getElementById('birthplace_country_select');
+        let birthplace_sity_select = document.getElementById('birthplace_sity_select');
+        let person_father_select = document.getElementById('person_father_select');
+        let person_mother_select = document.getElementById('person_mother_select');
+        let person_current_spouse_select = document.getElementById('person_current_spouse_select');
+        // Очистка при повторном открытии окна
+        $("#birthplace_country_select").empty();
+        $("#birthplace_sity_select").empty();
+        $("#person_father_select").empty();
+        let opt = document.createElement('option');
+        opt.value = -1;
+        opt.innerHTML = 'Unknown';
+        person_father_select.appendChild(opt);
+        $("#person_mother_select").empty();
+        let opt1 = document.createElement('option');
+        opt1.value = -1;
+        opt1.innerHTML = 'Unknown';
+        person_mother_select.appendChild(opt1);
+        let opt2 = document.createElement('option');
+        opt2.value = -1;
+        opt2.innerHTML = 'Unknown';
+        person_current_spouse_select.appendChild(opt2);
         //	    Заполнить список стран
         Object.entries(country_dict).forEach(([key, value]) => {
             let opt = document.createElement('option');
@@ -46,6 +87,30 @@ $(document).ready(function() {
         //        Сделать Russia активной по умолчанию
         $('option[value="Russia"]').attr('selected', 'selected').parent().focus();
         $('option[value="Perm"]').attr('selected', 'selected').parent().focus();
+        // Люди мужского пола
+        let mans = get_person_by_gender('M')
+        for (let i = 0; i < mans.length; i++) {
+            let opt = document.createElement('option');
+            opt.value = mans[i]['id'];
+            opt.innerHTML = mans[i]['family'] + ' ' + mans[i]['name'];
+            person_father_select.appendChild(opt);
+        }
+        // Люди женского пола
+        let womens = get_person_by_gender('F')
+        for (let i = 0; i < womens.length; i++) {
+            let opt = document.createElement('option');
+            opt.value = womens[i]['id'];
+            opt.innerHTML = womens[i]['family'] + ' ' + womens[i]['name'];
+            person_mother_select.appendChild(opt);
+        }
+        // Все Люди
+        let peoples = get_person_by_gender('None')
+        for (let i = 0; i < peoples.length; i++) {
+            let opt = document.createElement('option');
+            opt.value = peoples[i]['id'];
+            opt.innerHTML = peoples[i]['family'] + ' ' + peoples[i]['name'];
+            person_current_spouse_select.appendChild(opt);
+        }
         //        Открыть модальное окно
         $('#Person_modal').toggle();
     })
@@ -58,16 +123,27 @@ $(document).ready(function() {
         let gender = $('#person_gender').find(":selected").val()
         let country = $('#birthplace_country_select').find(":selected").val();
         let sity = $('#birthplace_sity_select').find(":selected").val();
+        let father_id = $('#person_father_select').find(":selected").val();
+        if (!!!father_id)
+            father_id = '-1'
+        let mother_id = $('#person_mother_select').find(":selected").val();
+        if (!!!mother_id)
+            mother_id = '-1'
+        let spouse_id = $('#person_current_spouse_select').find(":selected").val();
+        if (!!!spouse_id)
+            spouse_id = '-1'
 
         let json_table = {
             'family': family,
             'name': name,
             'gender': gender,
             'midlename': midlename,
-            'position': {
-                'country': country,
-                'sity': sity,
-            },
+            "mother_num": mother_id,
+            "father_num": father_id,
+            "spouse_num": spouse_id,
+            'position': {'country': country,
+                         'sity': sity,
+                         },
         }
         console.log(JSON.stringify(json_table))
         $.ajax({
