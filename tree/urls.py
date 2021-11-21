cb5b3bed68aaac45f64003c49cba4b285cd25bb2
Alexander <aleksandr__sokol@mail.ru>
2021-11-21 18:03:34 +0500

    feat: Add jwt token

--- tree/urls.py
+++ tree/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from rest_framework_simplejwt import views as jwt_views
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('family_tree.urls')),
-    # path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),  # получить
-    # path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),  # обновить
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),  # получить
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),  # обновить
 ]
