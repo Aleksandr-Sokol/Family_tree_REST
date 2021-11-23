1b8fb8b368f627ae2a3b9ce2f002a81e87307ac5
Alexander <aleksandr__sokol@mail.ru>
2021-11-23 13:33:37 +0500

    fix: Send mail message

--- family_tree/tasks.py
+++ family_tree/tasks.py
@@ -1,8 +1,16 @@
 from .models import Person
 from celery import shared_task
+from django.core.mail import EmailMessage
+from decouple import config
 
 
 @shared_task
 def persons_count():
     queryset = Person.objects.all()
-    return f'Person count is {len(queryset)}'
+    num_persons = len(queryset)
+    send_email_address = config('SENT_EMAIL_ADDRESS')
+    subject = f'Количество людей в базе'
+    body = f'На текущий момент в базе {num_persons} человек'
+    email = EmailMessage(subject, body, to=[send_email_address])
+    email.send()
+    return f'Person count is {num_persons}'
