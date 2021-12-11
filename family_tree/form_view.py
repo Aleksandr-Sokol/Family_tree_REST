7f741df96da111f60231e3e61e9ed704ab050bc4
Alexander <aleksandr__sokol@mail.ru>
2021-12-11 21:40:07 +0500

    delete: old file
    This functionality in application family_tree_view

--- family_tree/form_view.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'family_tree/index.pug')
-
-
-def tree(request):
-    return render(request, 'family_tree/tree.pug')
